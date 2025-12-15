5f7fb092d4946f9376fb8550bad40506cbbd7bd3
Zunaid <fakehotdog03@gmail.com>
2025-12-15 21:52:39 +1100

    make an install script to install all tmux plugins

---
+++ tmux/install.sh
@@ -0,0 +1,7 @@
+mkdir -p plugins
+
+mkdir -p plugins/tpm
+git clone https://github.com/tmux-plugins/tpm plugins/tpm
+
+mkdir -p plugins/catppuccin 
+git clone git@github.com:catppuccin/tmux.git plugins/catppuccin
